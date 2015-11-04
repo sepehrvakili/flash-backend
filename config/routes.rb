975b7012bd70c7945c7df08e7e3d3a258f653a91
heysep <sdvakili@gmail.com>
2015-11-04 17:41:10 -0500

    routes are signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'registrations/create'
+
+  get 'registrations/destroy'
+
+  get 'registrations/login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
